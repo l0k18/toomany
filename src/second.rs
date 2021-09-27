c9fda65bc7c6027c41939bd396725128779f50f8
Loki <david@dusk.network>
2021-09-27 17:48:43 +0300

    backtracking - using the Option and Box instead of crazy version

--- src/second.rs
+++ src/second.rs
@@ -1,17 +1,17 @@
 use std::mem;
 
-pub struct List<T> {
-    head: Link<T>,
+pub struct List {
+    head: Link,
 }
 
-type Link<T> = Option<Box<Node<T>>>;
+type Link = Option<Box<Node>>;
 
-struct Node<T> {
+struct Node {
     elem: i32,
-    next: Link<T>,
+    next: Link,
 }
 
-impl List<T> {
+impl List {
     pub fn new() -> Self {
         List { head: None }
     }
@@ -19,36 +19,37 @@ impl List<T> {
     pub fn push(&mut self, elem: i32) {
         let new_node = Box::new(Node {
             elem: elem,
-            next: self.head.take(),
+            next: mem::replace(&mut self.head, None),
         });
         self.head = Some(new_node);
     }
 
     pub fn pop(&mut self) -> Option<i32> {
-        self.head.take().map(|node| {
-            self.head = node.next;
-            node.elem
-        })
-    }
-}
-
-impl Drop for List<T> {
-    fn drop(&mut self) {
-        let mut cur_link = mem::replace(&mut self.head, None);
-        // `while let` == "do this thing until this pattern doesn't match"
-        while let Some(mut boxed_node) = cur_link {
-            cur_link = boxed_node.next.take();
-            // boxed_node goes out of scope and gets dropped here;
-            // but its Node's `next` field has been set to Link::Empty
-            // so no unbounded recursion occurs.
+        match mem::replace(&mut self.head, None) {
+            None => None,
+            Some(node) => {
+                self.head = node.next;
+                Some(node.elem)
+            }
         }
     }
 }
 
+impl Drop for List {
+    fn drop(&mut self) {
+        let mut cur_link = mem::replace(&mut self.head, None);
+        // `while let` == "do this thing until this pattern doesn't match"
+        while let Some(mut boxed_node) = cur_link {
+            cur_link = mem::replace(&mut boxed_node.next, None);
+            // boxed_node goes out of scope and gets dropped here;
+            // but its Node's `next` field has been set to None
+            // so no unbounded recursion occurs.
+        }
+    }
+}
 #[cfg(test)]
 mod test {
     use crate::first::List;
-
     #[test]
     fn basics() {
         let mut list = List::new();
